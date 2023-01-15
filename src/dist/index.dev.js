e1cb0634076baf0f40801f820fa11116df3f682f
MrEpoch <109413478+MrEpoch@users.noreply.github.com>
2023-01-15 17:17:11 +0100

    commit

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -6,9 +6,50 @@ require("./style.css");
 
 var methods = _interopRequireWildcard(require("./components/methods"));
 
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+function _getRequireWildcardCache() {
+  if (typeof WeakMap !== "function") return null;
+  var cache = new WeakMap();
+  _getRequireWildcardCache = function _getRequireWildcardCache() {
+    return cache;
+  };
+  return cache;
+}
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  }
+  if (
+    obj === null ||
+    (_typeof(obj) !== "object" && typeof obj !== "function")
+  ) {
+    return { default: obj };
+  }
+  var cache = _getRequireWildcardCache();
+  if (cache && cache.has(obj)) {
+    return cache.get(obj);
+  }
+  var newObj = {};
+  var hasPropertyDescriptor =
+    Object.defineProperty && Object.getOwnPropertyDescriptor;
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      var desc = hasPropertyDescriptor
+        ? Object.getOwnPropertyDescriptor(obj, key)
+        : null;
+      if (desc && (desc.get || desc.set)) {
+        Object.defineProperty(newObj, key, desc);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+  }
+  newObj["default"] = obj;
+  if (cache) {
+    cache.set(obj, newObj);
+  }
+  return newObj;
+}
 
 var src = methods.divCreate("", "src");
 var navbar = document.createElement("nav");
@@ -21,32 +62,65 @@ var navMenu = {
   menu: methods.h4Create("Menu", "menu")
 };
 var text = {
-  MainInfo: "We are restaurant with long lasting tradition, we are famous for beautiful place, forest, mountains, rivers are us. For many years we bring best possible to your plates and with good prices, we restaurant One Time are one of most enjoyable things you on your little travelling adventure can have. ",
+  MainInfo:
+    "We are restaurant with long lasting tradition, we are famous for beautiful place, forest, mountains, rivers are us. For many years we bring best possible to your plates and with good prices, we restaurant One Time are one of most enjoyable things you on your little travelling adventure can have. ",
   timeHeader: "Running hours",
   openingInfo: ""
 };
 
 var contentRestaurant = function contentRestaurant() {
   var centerInfo = methods.divCreate("", "center-info");
-  var timeDiv = methods.divCreate(methods.h2Create(text.timeHeader, "time-header"), "time-div");
-  var aboutUs = methods.h2Create(text.MainInfo, "info-content");
-  var days = {
-    monday: "Monday:    8am - 6pm",
-    tuesday: "Tuesday:   8am - 6pm",
-    wednesday: "Wednesday: 8am - 6pm",
-    thursday: "Thursday:  8am - 6pm",
-    friday: "Friday:    8am - 7pm",
-    saturday: "Saturday:  8am - 10pm",
-    sunday: "Sunday:    8am - 10pm"
+  var aboutUs = methods.divCreate(
+    methods.h2Create(text.MainInfo, "info-content"),
+    "about-container"
+  );
+  var timeDiv = methods.divCreate(
+    methods.h2Create(text.timeHeader, "time-h2"),
+    "time-container"
+  );
+  var place = methods.divCreate(
+    methods.h2Create(text.place, "place-h2"),
+    "place-container"
+  );
+
+  var aboutLoad = function aboutLoad() {
+    return aboutUs;
   };
-  var dayPara = methods.pCreate(days, "none");
-  timeDiv.append(dayPara);
-  centerInfo.append(aboutUs, timeDiv);
-  console.log(days.friday);
+
+  var openingTime = function openingTime() {
+    var days = function days() {
+      var monday = methods.pCreate("Monday:    8am - 6pm", "monday");
+      var tuesday = methods.pCreate("Tuesday:   8am - 6pm", "tuesday");
+      var wednesday = methods.pCreate("Wednesday: 8am - 6pm", "wednesday");
+      var thursday = methods.pCreate("Thursday:  8am - 6pm", "thursday");
+      var friday = methods.pCreate("Friday:    8am - 7pm", "friday");
+      var saturday = methods.pCreate("Saturday:  8am - 10pm", "sunday");
+      var sunday = methods.pCreate("Sunday:    8am - 10pm", "saturday");
+      return [monday, tuesday, wednesday, thursday, friday, saturday, sunday];
+    };
+
+    for (var assign = 0; assign < 7; assign += 1) {
+      timeDiv.append(days()[assign]);
+    }
+
+    return timeDiv;
+  };
+
+  var placeLoad = function placeLoad() {
+    place.append(methods.pCreate(text.placeText, "text-place"));
+    return place;
+  };
+
+  centerInfo.append(aboutLoad(), openingTime(), placeLoad());
   return {
     centerInfo: centerInfo
   };
 };
+
+navbar.append(navMenu.logoContainer, navMenu.restaurant, navMenu.contact, navMenu.menu);
+content.append(contentRestaurant().centerInfo);
+src.append(navbar, content, footer);
+document.body.appendChild(src);
 
 navbar.append(navMenu.logoContainer, navMenu.restaurant, navMenu.contact, navMenu.menu);
 content.append(contentRestaurant().centerInfo);
